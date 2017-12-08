e4471a69c94dc36a30bd34aa4c0e58f1c0ecb7fa
Alonso Tacanga <alonso.tacanga@gmail.com>
2017-12-08 10:45:25 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Router} from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello
+      </div>
+    );
+  }
+}
+
+export default App;
